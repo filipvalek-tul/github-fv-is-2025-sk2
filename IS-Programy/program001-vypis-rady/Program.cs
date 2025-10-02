33b3f8f935c6cae3eb507a35ac6544199823629c
Filip Válek <filip.valek@tul.cz>
2025-10-02 14:05:35 +0200

    #3 Vypis řady, vstup první hodnoty

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,33 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("***********************************");
+    Console.WriteLine("******* Výpis čiselné řady ********");
+    Console.WriteLine("***********************************");
+    Console.WriteLine("***********************************");
+    Console.WriteLine("********* Filip Válek *************");
+    Console.WriteLine("***********************************");
+    Console.WriteLine();
+
+
+
+    // Vstup hodnoty do programu, ale špatně řešený
+    //Console.Write("Zadejte první čislo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup do hodnoty programu, řešený lépe
+    Console.WriteLine("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first)) ;
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte ho znovu");
+    }
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
