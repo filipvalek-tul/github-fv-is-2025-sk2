5b746742d6dcaf19cdc939dfaabd685d5554c291
Filip Válek <filip.valek@tul.cz>
2025-10-23 13:11:45 +0200

    #5 Cviko 6 serayeni dvou cisel

---
+++ IS-Programy/program002a-soucet-cifer/Program.cs
@@ -0,0 +1,41 @@
+﻿string again = "a";
+
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Výpis číselné řady *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Filip Válek ******************");
+    Console.WriteLine("************** 2.10.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte celé číslo: ");
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+    }
+
+    int suma = 0;
+    int numberBackup = number;
+    int digit; //zbytek
+
+    if (number < 0)
+    {
+        number = -number;
+    }
+
+    while (number >= 10)
+    {
+        digit = number % 10 //
+        number = (number - digit) / 10;
+        Console.WriteLine("Hodnota zbytku = {0}", digit)
+        suma = suma + digit;
+    }
+    //
+    Console.WriteLine
---
+++ IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string? again = "a";
+
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********** Seřazení dvou čisel *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Filip Válek ******************");
+    Console.WriteLine("************** 2.10.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte celé číslo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu A: ");
+    }
+
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu B: ");
+    }
+
+    Console.WriteLine();
+
+    int pom;
+    //Chceme seradit cisla vzestupne
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+    }
+
+    Console.WriteLine("********************************************");
+    Console.WriteLine($"Seřazená čisla: {a}, {b}");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
