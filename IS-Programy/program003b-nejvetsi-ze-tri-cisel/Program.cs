e89d3e13bf57f909c4a537208629877efd4b6aa8
Filip Válek <filip.valek@tul.cz>
2025-11-06 12:59:07 +0100

    #6 Generaton vychoyi projekt

--- IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -23,7 +23,40 @@ while (again == "a")
         Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu A: ");
     }
 
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu B: ");
+    }
 
+    Console.Write("Zadejte celé číslo C: ");
+    int c;
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu C: ");
+    }
+
+    Console.WriteLine();
+
+    int pom;
+    //Chceme seradit cisla vzestupne
+    if (a > b)
+    {
+        if (a > c)
+            Console.WriteLine($"Největší čislo je A = {a}");
+        else
+            Console.WriteLine($"Největší čislo je C = {c}");
+    }
+    else
+    {
+        if (b > c)
+            Console.WriteLine($"Největší čislo je B = {b}");
+        else
+            Console.WriteLine($"Největší čislo je C = {c}");
+    }
+
+    Console.WriteLine("********************************************");
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
     again = Console.ReadLine();
--- IS-Programy/program004a-obdelnik/Program.cs
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -16,12 +16,33 @@ while (again == "a")
     Console.WriteLine();
 
     // Vstup hodnoty do programu, řešený lépe
-    Console.Write("Zadejte celé číslo A: ");
-    int a;
-    while (!int.TryParse(Console.ReadLine(), out a))
+    Console.Write("Zadejte šířku obdelníka: ");
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu A: ");
+        Console.Write("Nezadali jste celé číslo. Zadejte šířku obdelníka: ");
     }
+
+    Console.Write("Zadejte výšku obdelníka: ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte výšku obdelníka: ");
+    }
+
+    for (int i = 1; i <= height; i++)
+    {
+
+        for (int j = 1; j <= width; j++)
+        {
+            Console.Write("*");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(150));
+        }
+        Console.WriteLine();
+    }
+
+
+
 
 
     Console.WriteLine();
