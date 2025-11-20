8a25cd4b49e7a84edfa4f56a104e767241dcd405
Filip Válek <filip.valek@tul.cz>
2025-11-20 14:02:30 +0100

    #8 nevim co delam

--- IS-Programy/program007a-bubble-sort/Program.cs
+++ IS-Programy/program007a-bubble-sort/Program.cs
@@ -17,40 +17,44 @@ while (again == "a")
     Console.WriteLine("********************************************");
     Console.WriteLine();
 
-    // Vstup hodnoty do programu, řešený lépe
-    Console.Write("Zadejte počet generovaných čisel(celé číslo) ");
+    Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
     int n;
     while (!int.TryParse(Console.ReadLine(), out n))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu A: ");
+        Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu: ");
     }
 
-    Console.Write("Zadejte dolní mez (celé číslo) ");
-    int lowerBound;
+
+    Console.Write("Zadejte dolní mez (celé číslo): ");
+    int lowerBound; // Dolní mez
     while (!int.TryParse(Console.ReadLine(), out lowerBound))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu ");
+        Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu: ");
     }
 
-    Console.Write("Zadejte horní mez (celé číslo) ");
-    int upperBound;
+
+
+    Console.Write("Zadejte horní mez (celé číslo): ");
+    int upperBound; // Horní mez
     while (!int.TryParse(Console.ReadLine(), out upperBound))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu ");
+        Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu: ");
     }
 
+
     Console.WriteLine();
-    Console.WriteLine("********************************************");
+    Console.WriteLine("==========================================================");
     Console.WriteLine("Zadané hodnoty:");
-    Console.WriteLine("Počet čisel: {0},Dolní mez: {1}, Horní mez: {2}", n, lowerBound, upperBound);
-    Console.WriteLine("********************************************");
+    Console.WriteLine("Počet čísel: {0}, Dolní mez: {1}, Horní mez: {2}", n, lowerBound, upperBound);
+    Console.WriteLine("==========================================================");
     Console.WriteLine();
 
-    //deklarace pole (array)
+    // deklarace pole (array)
     int[] myRandNumbs = new int[n];
 
-    // připrava pro využití třidy Random
-    Random myRandNumb = new Random(15);
+    // příprava pro využití třídy Random
+    Random myRandNumb = new Random();
+    //Random myRandNumb = new Random(15);
 
     Console.WriteLine();
     Console.WriteLine("Náhodná čísla: ");
@@ -58,37 +62,36 @@ while (again == "a")
     {
         myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound + 1);
         Console.Write("{0}; ", myRandNumbs[i]);
-
     }
 
     Stopwatch myStopwatch = new Stopwatch();
 
-    int compare = 0;
-    int change = 0;
+    int compare = 0;  // počet porovnávání
+    int change = 0;   // počet výměn
 
     myStopwatch.Start();
     for (int i = 0; i < n - 1; i++)
     {
-        //tento cyklus musi zajistit porovnavani dvou sousednich hodnot
-        //musi dale zajistit abz se zmensoval poct porovnavanych hodnot
+        // tento cyklus musí zajisti porovnávání dvou soudedních hodnot
+        // musí dále zajistit, aby se zmenšoval počet porovnávaných hodnot
         for (int j = 0; j < n - i - 1; j++)
         {
-            compare++;
             if (myRandNumbs[j] > myRandNumbs[j + 1])
             {
-                int tpm = myRandNumbs[j + 1];
+                int tmp = myRandNumbs[j + 1];
                 myRandNumbs[j + 1] = myRandNumbs[j];
                 myRandNumbs[j] = tmp;
                 change++;
             }
+            compare++;
         }
-
     }
     myStopwatch.Stop();
 
     Console.WriteLine();
-    Console.WriteLine("********************************************");
-    Console.WriteLine("Serazene pole");
+    Console.WriteLine();
+    Console.WriteLine("==================================");
+    Console.WriteLine("Seřazené pole: ");
     for (int i = 0; i < n; i++)
     {
         Console.Write("{0}; ", myRandNumbs[i]);
@@ -97,10 +100,12 @@ while (again == "a")
     Console.WriteLine();
     Console.WriteLine();
     Console.WriteLine();
-    Console.WriteLine($"Pocet porovnani: {compare}");
-    Console.WriteLine($"Pocet zmen: {change}");
+    Console.WriteLine($"Počet porovnání: {compare}");
+    Console.WriteLine($"Počet výměn: {change}");
     Console.WriteLine();
-    Console.WriteLine("cas serazeni cisel pomoci BS: {0}", myStopwatch.Elapsed);
+    Console.WriteLine("Čas seřazení čísel pomocí BS: {0}", myStopwatch.Elapsed);
+
+
 
 
 
--- IS-Programy/program007b-bubble-sort/Program.cs
+++ IS-Programy/program007b-bubble-sort/Program.cs
@@ -1,2 +1,116 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+
+
+string again = "a";
+
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("***** Generátor pseudonáhodných čisel ******");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Filip Válek ******************");
+    Console.WriteLine("************** 6.11.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+    int n;
+    while (!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu: ");
+    }
+
+
+    Console.Write("Zadejte dolní mez (celé číslo): ");
+    int lowerBound; // Dolní mez
+    while (!int.TryParse(Console.ReadLine(), out lowerBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu: ");
+    }
+
+
+
+    Console.Write("Zadejte horní mez (celé číslo): ");
+    int upperBound; // Horní mez
+    while (!int.TryParse(Console.ReadLine(), out upperBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu: ");
+    }
+
+
+    Console.WriteLine();
+    Console.WriteLine("==========================================================");
+    Console.WriteLine("Zadané hodnoty:");
+    Console.WriteLine("Počet čísel: {0}, Dolní mez: {1}, Horní mez: {2}", n, lowerBound, upperBound);
+    Console.WriteLine("==========================================================");
+    Console.WriteLine();
+
+    // deklarace pole (array)
+    int[] myRandNumbs = new int[n];
+
+    // příprava pro využití třídy Random
+    Random myRandNumb = new Random();
+    //Random myRandNumb = new Random(15);
+
+    Console.WriteLine();
+    Console.WriteLine("Náhodná čísla: ");
+    for (int i = 0; i < n; i++)
+    {
+        myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound + 1);
+        Console.Write("{0}; ", myRandNumbs[i]);
+    }
+
+    Stopwatch myStopwatch = new Stopwatch();
+
+    int compare = 0;  // počet porovnávání
+    int change = 0;   // počet výměn
+
+    myStopwatch.Start();
+    for (int i = 0; i < n - 1; i++)
+    {
+        // tento cyklus musí zajisti porovnávání dvou soudedních hodnot
+        // musí dále zajistit, aby se zmenšoval počet porovnávaných hodnot
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (myRandNumbs[j] < myRandNumbs[j + 1])
+            {
+                int tmp = myRandNumbs[j + 1];
+                myRandNumbs[j + 1] = myRandNumbs[j];
+                myRandNumbs[j] = tmp;
+                change++;
+            }
+            compare++;
+        }
+    }
+    myStopwatch.Stop();
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("==================================");
+    Console.WriteLine("Seřazené pole: ");
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("{0}; ", myRandNumbs[i]);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine($"Počet porovnání: {compare}");
+    Console.WriteLine($"Počet výměn: {change}");
+    Console.WriteLine();
+    Console.WriteLine("Čas seřazení čísel pomocí BS: {0}", myStopwatch.Elapsed);
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
